b2b8431dbfe0b8b505611e9641c6557830b580f9
enesctnn <enespcetin@gmail.com>
2024-03-31 06:06:17 +0300

    Folder Location change for deployment build time fixes.

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,67 +1,67 @@
 export const icons = {
   '01d': {
-    src: 'http://localhost:5173/src/assets/icons/ClearDay.png',
+    src: '/icons/ClearDay.png',
     alt: 'clear day weather icon',
   },
   '01n': {
-    src: 'http://localhost:5173/src/assets/icons/ClearNight.png',
+    src: '/icons/ClearNight.png',
     alt: 'clear night weather icon',
   },
   '02d': {
-    src: 'http://localhost:5173/src/assets/icons/FewcloudsDay.png',
+    src: '/icons/FewcloudsDay.png',
     alt: 'fewclouds day weather icon',
   },
   '02n': {
-    src: 'http://localhost:5173/src/assets/icons/FewcloudsNight.png',
+    src: '/icons/FewcloudsNight.png',
     alt: 'fewclouds night weather icon',
   },
   '03d': {
-    src: 'http://localhost:5173/src/assets/icons/CloudyDay.png',
+    src: '/icons/CloudyDay.png',
     alt: 'cloudy day weather icon',
   },
   '03n': {
-    src: 'http://localhost:5173/src/assets/icons/CloudyNight.png',
+    src: '/icons/CloudyNight.png',
     alt: 'cloudy night weather icon',
   },
   '04d': {
-    src: 'http://localhost:5173/src/assets/icons/BrokenDay.png',
+    src: '/icons/BrokenDay.png',
     alt: 'broken clouds day weather icon',
   },
   '04n': {
-    src: 'http://localhost:5173/src/assets/icons/BrokenNight.png',
+    src: '/icons/BrokenNight.png',
     alt: 'broken clouds night weather icon',
   },
 
   '09d': {
-    src: 'http://localhost:5173/src/assets/icons/RainDay.png',
+    src: '/icons/RainDay.png',
     alt: 'rain day weather icon',
   },
   '09n': {
-    src: 'http://localhost:5173/src/assets/icons/RainNight.png',
+    src: '/icons/RainNight.png',
     alt: 'rain night weather icon',
   },
   '10d': {
-    src: 'http://localhost:5173/src/assets/icons/RainDay.png',
+    src: '/icons/RainDay.png',
     alt: 'rain day weather icon',
   },
   '10n': {
-    src: 'http://localhost:5173/src/assets/icons/RainNight.png',
+    src: '/icons/RainNight.png',
     alt: 'rain night weather icon',
   },
   '11d': {
-    src: 'http://localhost:5173/src/assets/icons/StormDay.png',
+    src: '/icons/StormDay.png',
     alt: 'stormy day weather icon',
   },
   '11n': {
-    src: 'http://localhost:5173/src/assets/icons/StormNight.png',
+    src: '/icons/StormNight.png',
     alt: 'stormy night weather icon',
   },
   '13d': {
-    src: 'http://localhost:5173/src/assets/icons/SnowDay.png',
+    src: '/icons/SnowDay.png',
     alt: 'snowy day weather icon',
   },
   '13n': {
-    src: 'http://localhost:5173/src/assets/icons/SnowNight.png',
+    src: '/icons/SnowNight.png',
     alt: 'snowy night weather icon',
   },
   '50d': {
@@ -76,75 +76,75 @@ export const icons = {
 
 export const bgs = {
   '01d': {
-    src: 'http://localhost:5173/src/assets/bg/clearday.png',
+    src: '/bg/clearday.png',
     alt: 'clear day weather background image',
   },
   '01n': {
-    src: 'http://localhost:5173/src/assets/bg/clearnight.png',
+    src: '/bg/clearnight.png',
     alt: 'clear night weather background image',
   },
   '02d': {
-    src: 'http://localhost:5173/src/assets/bg/fewcloudsday.png',
+    src: '/bg/fewcloudsday.png',
     alt: 'fewclouds day weather background image',
   },
   '02n': {
-    src: 'http://localhost:5173/src/assets/bg/fewcloudsnight.png',
+    src: '/bg/fewcloudsnight.png',
     alt: 'fewclouds night weather background image',
   },
   '03d': {
-    src: 'http://localhost:5173/src/assets/bg/cloudyday.png',
+    src: '/bg/cloudyday.png',
     alt: 'cloudy day weather background image',
   },
   '03n': {
-    src: 'http://localhost:5173/src/assets/bg/cloudynight.png',
+    src: '/bg/cloudynight.png',
     alt: 'cloudy night weather background image',
   },
   '04d': {
-    src: 'http://localhost:5173/src/assets/bg/cloudyday.png',
+    src: '/bg/cloudyday.png',
     alt: 'broken clouds day weather background image',
   },
   '04n': {
-    src: 'http://localhost:5173/src/assets/bg/cloudynight.png',
+    src: '/bg/cloudynight.png',
     alt: 'broken clouds weather background image',
   },
   '09d': {
-    src: 'http://localhost:5173/src/assets/bg/rainday.png',
+    src: '/bg/rainday.png',
     alt: 'rain day weather background image',
   },
   '09n': {
-    src: 'http://localhost:5173/src/assets/bg/rainnight.png',
+    src: '/bg/rainnight.png',
     alt: 'rain night weather background image',
   },
   '10d': {
-    src: 'http://localhost:5173/src/assets/bg/rainday.png',
+    src: '/bg/rainday.png',
     alt: 'rain day weather background image',
   },
   '10n': {
-    src: 'http://localhost:5173/src/assets/bg/rainnight.png',
+    src: '/bg/rainnight.png',
     alt: 'rain night weather background image',
   },
   '11d': {
-    src: 'http://localhost:5173/src/assets/bg/stormday.png',
+    src: '/bg/stormday.png',
     alt: 'storm day weather background image',
   },
   '11n': {
-    src: 'http://localhost:5173/src/assets/bg/stormnight.png',
+    src: '/bg/stormnight.png',
     alt: 'storm night weather background image',
   },
   '13d': {
-    src: 'http://localhost:5173/src/assets/bg/cloudyday.png',
+    src: '/bg/cloudyday.png',
     alt: 'snowy night weather background image',
   },
   '13n': {
-    src: 'http://localhost:5173/src/assets/bg/cloudynight.png',
+    src: '/bg/cloudynight.png',
     alt: 'snowy night weather background image',
   },
   '50d': {
-    src: 'http://localhost:5173/src/assets/bg/cloudyday.png',
+    src: '/bg/cloudyday.png',
     alt: 'misty day weather background image',
   },
   '50n': {
-    src: 'http://localhost:5173/src/assets/bg/cloudynight.png',
+    src: '/bg/cloudynight.png',
     alt: 'misty night weather background image',
   },
 };
