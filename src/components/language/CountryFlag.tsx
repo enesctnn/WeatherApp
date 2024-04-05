11dd69ac010f477355feb045e16ddeb459721dd1
enesctnn <enespcetin@gmail.com>
2024-04-05 20:09:25 +0300

    styling changes on button. fixed bug on country flag

--- src/components/language/CountryFlag.tsx
+++ src/components/language/CountryFlag.tsx
@@ -3,11 +3,7 @@ import ReactCountryFlag from 'react-country-flag';
 export const CountryFlag = ({ language }: { language: string }) => (
   <ReactCountryFlag
     className="mx-2 text-xl"
-    countryCode={
-      language.split('-').join('').slice(2, 4) || language.includes('tr')
-        ? 'tr'
-        : 'us'
-    }
+    countryCode={language.includes('tr') ? 'tr' : 'us'}
     svg
   />
 );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     <button
       className={cn(
         className,
-        'disabled:pointer-events-none disabled:opacity-50 disabled:scale-90 px-2 sm:px-4 py-1 bg-ellipse from-blue-light to-gray-500 rounded-md text-heading-sm md:text-heading-md !text-gray-100 transition-all'
+        'disabled:pointer-events-none enabled:opacity-50 enabled:scale-90 px-2 sm:px-4 py-1 bg-ellipse from-blue-light to-gray-500 rounded-md text-heading-sm md:text-heading-md !text-gray-100 transition-all enabled:hover:opacity-100 enabled:hover:scale-100 enabled:active:scale-110'
       )}
       ref={ref}
       {...props}
