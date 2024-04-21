8326e3eeb59f26c43e84fe3fdfa643b6670098c5
Muhammet Enes Çetin <enespcetin@gmail.com>
2024-04-21 17:57:06 +0300

    Favorites list items set to thinner also in bigger screensizes.

--- src/components/favorites/FavoritesList.tsx
+++ src/components/favorites/FavoritesList.tsx
@@ -21,11 +21,11 @@ export function FavoritesList({ favorites }: { favorites: FavoritesT }) {
         opacity: 0,
       }}
       whileHover={{ x: -10, opacity: 1 }}
-      className="weather-heading max-lg:max-w-30 flex items-center gap-x-1 overflow-ellipsis rounded-l-full border-b-2 border-l-2 border-t-2 border-gray-200 bg-blue-light px-2 text-gray-100 dark:border-gray-600 dark:bg-gray-500 md:border-b-4 md:border-l-4 md:border-t-4"
+      className="weather-heading max-lg:max-w-30 flex items-center gap-x-1 overflow-ellipsis rounded-l-full border-b-2 border-l-2 border-t-2 border-gray-200 bg-blue-light px-2 text-gray-100 dark:border-gray-600 dark:bg-gray-500"
     >
       <Link
         to={`/weather/${key}`}
-        className="user-drag-none flex h-full w-full items-center whitespace-pre-line py-1 md:h-12"
+        className="user-drag-none flex h-full w-full items-center whitespace-pre-line py-1"
       >
         {favorites[key]}
       </Link>
